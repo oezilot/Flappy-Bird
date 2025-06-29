4de315a11813b827028163c400cb5d62e1bfa6c4
oezilot <zoe.flumini@gmail.com>
2025-06-29 03:00:58 +0200

    xInWorld funktion

--- Assets/ObstacleScript.cs
+++ Assets/ObstacleScript.cs
@@ -8,16 +8,21 @@ public class ObstacleScript : MonoBehaviour
     // object = 
     bool tracker;
     float speed = -0.01f;
-    float xInWorld;
+    float screenWidthWorld;
 
-  
+    float xInWorld(float xValue) {
+        float xWorld = (Camera.main.ScreenToWorldPoint(new Vector3((xValue), Screen.height / 2, Camera.main.nearClipPlane))).x;
+        return xWorld;
+    }
+
+
     void Start() {
         tracker = false;
 
         // pixels in units umwandeln damit das spawnen unabhängig der bildschirmgrösse passieren kann!
         Debug.Log("Screen Width : " + Screen.width);
-        xInWorld = (Camera.main.ScreenToWorldPoint(new Vector3((Screen.width), Screen.height / 2, Camera.main.nearClipPlane))).x;
-        Debug.Log("World Width : " + xInWorld);
+        screenWidthWorld = xInWorld(Screen.width);
+        Debug.Log("World Width : " + xInWorld(Screen.width));
     }
 
     // Update is called once per frame
@@ -26,12 +31,12 @@ public class ObstacleScript : MonoBehaviour
         // object moves across the x-axis
         gameObject.transform.position = new Vector3(gameObject.transform.position.x+speed, gameObject.transform.position.y, gameObject.transform.position.z);
 
-        if (gameObject.transform.position.x < -xInWorld) {
+        if (gameObject.transform.position.x < -screenWidthWorld) {
 
             if (tracker == false) { // versichert dass nur einmal ein neuer baum gespawned wird!
                 Debug.Log(gameObject.transform.position.x);
                 // ein neues objectr spawnen zu beginn
-                GameObject clone = Instantiate(gameObject, new Vector3(xInWorld, gameObject.transform.position.y, gameObject.transform.position.z), transform.rotation);
+                GameObject clone = Instantiate(gameObject, new Vector3(screenWidthWorld, gameObject.transform.position.y, gameObject.transform.position.z), transform.rotation);
                 tracker = true; // somit kann diese aktion nur 1mal durchgeführt werden!
                 Destroy(gameObject);
             }
